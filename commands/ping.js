1f48fc33e6fd604171a8f2525b8d358f99f9c2ff
axelmy3128 <a.milliery@icloud.com>
2021-04-16 21:58:53 +0200

    Basic setup with a command whitelisting system

---
+++ commands/ping.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'ping',
+    description: 'Responds with pong',
+    execute(message, args){
+        message.channel.send('pong')
+    }
+}
---
+++ main.js
@@ -0,0 +1,38 @@
+//Importing modules and json files
+const Discord = require('discord.js')
+const DiscordAPIData = require('./discordAPI.json')
+const BotSettings = require('./BotSettings.json')
+const fs = require('fs')
+
+//Initializing client
+const client = new Discord.Client()
+
+client.commands = new Discord.Collection()
+
+//Making sure all the commands files are javascript
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
+
+//Loading all the commands into the client
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`)
+
+    client.commands.set(command.name, command)
+}
+
+client.once('ready', () => {
+    console.log('Client ready')
+})
+
+client.on('message', message => {
+    //Returning if the message is not a command, or if the bot sent it
+    if(!message.content.startsWith(BotSettings.prefix) || message.author.bot) return
+
+    const args = message.content.slice(BotSettings.prefix.length).split(/ +/)
+    const command = args.shift().toLowerCase()
+    
+    if(BotSettings.commandsWhitelist.includes(command)){
+        client.commands.get(command).execute(message, args)
+    }
+})
+
+client.login(DiscordAPIData.accessToken)
